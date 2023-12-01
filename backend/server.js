ed35816e0286305325f226bd258a9cd549dd11c8
somnathp2757 <139745338+somnathp2757@users.noreply.github.com>
2023-12-01 12:05:50 +0530

    Update server.js
    username and password for connecting mongodb

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,22 @@ const mongoose = require("mongoose");
 const port = 3001;
 const routes = require("./routes");
 
+const username = "root";
+const password = "EPQ46jw3iL";
+
 main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect("mongodb://10.4.0.14:27017/todos", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
+    auth: { user: username, password: password },
   });
+
   const app = express();
   app.use(cors());
   app.use(express.json());
   app.use("/api", routes);
-  app.get('/', (req, res) => {
-    res.status(200).send('Welcome to the home page!');
-});
 
   app.listen(port, () => {
     console.log(`Server is listening on port: ${port}`);
